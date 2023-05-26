73da8182d7a03acefa9062c9e0cab70f9a18da06
William Jones <william.jones@physics.ox.ac.uk>
2023-05-26 19:01:23 +0100

    Add ensemble support

--- src/get_jasmin_era5/core.py
+++ src/get_jasmin_era5/core.py
@@ -11,6 +11,7 @@ class Find_era5:
         self._init_vars()
         self.pl = Pressure_levels_era5()
         self.gz = Geopotential_levels_era5()
+        self.enda = Ensemble_era5()
         
     def _init_vars(self):
         self.path = pathlib.Path("/badc/ecmwf-era5/data/")
@@ -60,6 +61,10 @@ class Find_era5:
                     sel["latitude"] = slice(90, sel["latitude"].stop, sel["latitude"].step)
                 if sel["latitude"].stop is None:
                     sel["latitude"] = slice(sel["latitude"].stop, -90, sel["latitude"].step)
+        if len(args) > 5:
+            model = args[5]
+        else:
+            model = "oper"
         
         if isinstance(date, slice):
             if date.step is None:
@@ -74,11 +79,11 @@ class Find_era5:
             
         if isinstance(var, str):
             var = [var]
-        files = sum([self.find_files(v, dates) for v in var if v not in self._INVARIANTS], [])
+        files = sum([self.find_files(v, dates, model=model) for v in var if v not in self._INVARIANTS], [])
         if len(files) > 0:
             ds = xr.open_mfdataset(files, combine="by_coords")
             ds = ds.sel({k:v for k,v in sel.items() if k in ds.coords})
-        invar_files = sum([self.find_files(v, dates) for v in var if v in self._INVARIANTS], [])
+        invar_files = sum([self.find_files(v, dates, model=model) for v in var if v in self._INVARIANTS], [])
         if len(invar_files) > 0:
             invar_ds = xr.open_mfdataset(invar_files, combine="by_coords").squeeze(drop=True)
             invar_ds = invar_ds.sel({k:v for k,v in sel.items() if k in invar_ds.coords})
@@ -93,7 +98,7 @@ class Find_era5:
         if var in self._INVARIANTS:
             return self.find_invariant(var)
         else:
-            return sum([self.find_single_file(var, date) for date in dates], [])
+            return sum([self.find_single_file(var, date, model=model) for date in dates], [])
     
     def find_invariant(self, var: str) -> list[str]:
         date = self._INVARIANT_DATE
@@ -104,8 +109,12 @@ class Find_era5:
         return files
         
     def find_single_file(self, var: str, date: datetime, model: str = "oper") -> list[str]:
+        if model == "enda":
+            level_type = "em_sfc"
+        else:
+            level_type = "*"
         files = sorted(list(self.path.glob(
-            f"{model}/*/{date.year:04d}/{date.month:02d}/{date.day:02d}/ecmwf-era5_{model}_*_{date.year:04d}{date.month:02d}{date.day:02d}{date.hour:02d}*.{var}.nc"
+            f"{model}/{level_type}/{date.year:04d}/{date.month:02d}/{date.day:02d}/ecmwf-era5_{model}_*_{date.year:04d}{date.month:02d}{date.day:02d}{date.hour:02d}*.{var}.nc"
         )))
         return files
     
@@ -162,4 +171,89 @@ class Geopotential_levels_era5(Find_era5):
             dims=("time", "level", "latitude", "longitude"),
             coords={"level":range(1,138), "time":ds.time, "longitude":ds.longitude, "latitude":ds.latitude}
         ).sel(sel)
+
+class Ensemble_era5(Find_era5):
+    def __init__(self):
+        self._init_vars()
     
+    def __getitem__(self, args):
+        var = args[0]
+        date = args[1]
+        sel = {}
+        if len(args) > 2 and args[2] is not None:
+            sel["level"] = args[2]
+            if isinstance(sel["level"], slice):
+                if sel["level"].start is None:
+                    sel["level"] = slice(1, sel["level"].stop, sel["level"].step)
+                if sel["level"].stop is None:
+                    sel["level"] = slice(sel["level"].start, 137, sel["level"].step)
+        if len(args) > 3 and args[3] is not None:
+            sel["longitude"] = args[3]
+            if isinstance(sel["longitude"], slice):
+                if sel["longitude"].start is None:
+                    sel["longitude"] = slice(0, sel["longitude"].stop, sel["longitude"].step)
+                if sel["longitude"].stop is None:
+                    sel["longitude"] = slice(sel["longitude"].start, 360, sel["longitude"].step)
+        if len(args) > 4 and args[4] is not None:
+            if isinstance(args[4], slice) and args[4].stop > args[4].start:
+                sel["latitude"] = slice(args[4].stop, args[4].start, args[4].step)
+            else:
+                sel["latitude"] = args[4]
+            if isinstance(sel["latitude"], slice):
+                if sel["latitude"].start is None:
+                    sel["latitude"] = slice(90, sel["latitude"].stop, sel["latitude"].step)
+                if sel["latitude"].stop is None:
+                    sel["latitude"] = slice(sel["latitude"].stop, -90, sel["latitude"].step)
+        
+        if isinstance(date, slice):
+            if date.step is None:
+                freq = "1H"
+            else:
+                freq = date.step
+            dates = pd.date_range(
+                pd.to_datetime(date.start), pd.to_datetime(date.stop), inclusive="left", freq=freq
+            ).to_pydatetime().tolist()
+        else:
+            dates = [pd.to_datetime(date).to_pydatetime()]
+            
+        if isinstance(var, str):
+            var = [var]
+        files = [self.find_files(v, dates, model="enda") for v in var if v not in self._INVARIANTS]
+        if len(files) > 0:
+            ds_list = [[xr.open_mfdataset(f, combine="nested", concat_dim=("ensemble_member")).assign_coords({"ensemble_member": range(1, len(f)+1)}) for f in fl if len(fl)] for fl in files]
+            ds = xr.merge([xr.concat(dsl, dim="time") for dsl in ds_list if len(dsl)])
+            ds = ds.sel({k:v for k,v in sel.items() if k in ds.coords})
+        invar_files = sum([self.find_files(v, dates, model="oper") for v in var if v in self._INVARIANTS], [])
+        if len(invar_files) > 0:
+            invar_ds = xr.open_mfdataset(invar_files, combine="by_coords").squeeze(drop=True)
+            invar_ds = invar_ds.sel({k:v for k,v in sel.items() if k in invar_ds.coords})
+            if len(files) > 0:
+                for invar in invar_ds.data_vars:
+                    ds[invar] = invar_ds[invar]
+            else:
+                ds = invar_ds
+        return ds
+    
+    def find_files(self, var: str, dates: list[datetime], model: str = "enda") -> list[str]:
+        if var in self._INVARIANTS:
+            return self.find_invariant(var)
+        else:
+            return [self.find_single_file(var, date, model=model) for date in dates]
+    
+    def find_invariant(self, var: str) -> list[str]:
+        date = self._INVARIANT_DATE
+        files = sorted(list(self.path.glob(
+            f"invariants/ecmwf-era5_oper_an_sfc_{date.year:04d}{date.month:02d}{date.day:02d}0000.{var}.inv.nc"
+        )))
+        
+        return files
+        
+    def find_single_file(self, var: str, date: datetime, model: str = "enda") -> list[str]:
+        if model == "enda":
+            level_type = "an_sfc"
+        else:
+            level_type = "*"
+        files = sorted(list(self.path.glob(
+            f"{model}/{level_type}/{date.year:04d}/{date.month:02d}/{date.day:02d}/ecmwf-era5_{model}_*_{date.year:04d}{date.month:02d}{date.day:02d}{date.hour:02d}*.{var}.nc"
+        )))
+        return files
